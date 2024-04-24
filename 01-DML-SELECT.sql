f002c81d397aeeccf576fc6c8ff51c5b6a221895
DHan0913 <eogml01023@gmail.com>
2024-04-24 16:17:59 +0900

    Update 01-DML-SELECT.sql
    '24.04.24 SQL

--- 01-DML-SELECT.sql
+++ 01-DML-SELECT.sql
@@ -14,44 +14,95 @@ Describe Locations;
 -- SELECT 
 
 -- * : 테이블 내 모든 컬럼 Projection, 테이블 설계시에 정의한 순서대로 
-SELECT * FROM employees;
+SELECT
+    *
+FROM
+    employees;
 
 -- 특정 컬럼만 Projection하고자 하면 열 목록을 명시
 
 -- employees 테이블의 First_name, phone_number, hire_date, salary 만 보고 싶다면
-SELECT first_name, phone_number, hire_date, salary FROM employees;
+SELECT
+    first_name,
+    phone_number,
+    hire_date,
+    salary
+FROM
+    employees;
 
 -- 사원의 이름, 성, 급여, 전화번호, 입사일 정보 출력 
-SELECT first_name, last_name, salary, phone_number, hire_date From employees;
+SELECT
+    first_name,
+    last_name,
+    salary,
+    phone_number,
+    hire_date
+FROM
+    employees;
 
 -- 사원 정보로부터 사번, 이름, 성 정보 출력
-SELECT employee_id, first_name, last_name FROM employees;
+SELECT
+    employee_id,
+    first_name,
+    last_name
+FROM
+    employees;
 
 -- 산술연산 : 기본적인 산술연산을 수행할 수 있다.
 -- 특정 테이블의 값이 아닌 시스템으로부터 데이터를 받아오고자 할때 : dual (가상테이블)
-SELECT 3.14159 * 10 * 10 FROM dual;
+SELECT
+    3.14159 * 10 * 10
+FROM
+    dual;
 
 -- 특정 컬럼의 값을 산술 연산에 포함
-SELECT first_name, salary, salary * 12 FROM employees;
+SELECT
+    first_name,
+    salary,
+    salary * 12
+FROM
+    employees;
 
 -- 다음 문장을 싱행해 봅시다.
-SELECT first_name, job_id, job_id*12 FROM employees; 
+SELECT
+    first_name,
+    job_id,
+    job_id * 12
+FROM
+    employees; 
 -- 오류의 원인 : job_id는 문자열 VARCHAR2 
 DESC employees;
 
 -- NULL
 -- 이름, 급여, 커미션 비율을 출력
-SELECT first_name, salary, commission_pct FROM employees;
+SELECT
+    first_name,
+    salary,
+    commission_pct
+FROM
+    employees;
 
 -- 이름, 커미션까지 포함한 급여를 출력
-SELECT first_name, salary, commission_pct, salary+salary*commission_pct 
-FROM employees;
+SELECT
+    first_name,
+    salary,
+    commission_pct,
+    salary + salary * commission_pct
+FROM
+    employees;
 -- NULL이 포함된 연산식의 결과는 NULL
 -- NULL을 처리하기 위한 함수 NVL이 필요 NVL (값, 값이 null일때 대체 값)
 
 -- NVL활용 대체값 계산
-SELECT first_name, salary, commission_pct, salary+salary * NVL (commission_pct, 0) 
-FROM employees;
+SELECT
+    first_name,
+    salary,
+    commission_pct,
+    salary + salary * nvl(
+        commission_pct, 0
+    )
+FROM
+    employees;
 
 
 -- NULL은 0이나 "" 와 다르게 빈 값이다.
@@ -67,26 +118,38 @@ FROM employees;
 
 -- 직원 아이디, 이름, 급여 출력
 -- 직원 아이디는 empNo, 이름은 f-name, 급여는 월급으로 표시 
-SELECT employee_id empNo -- 컬럼명 뒤에 별칭
-, first_name as "f-name" -- as 별칭, 특수문자 포함되면 ""로 묶음
-, salary "급 여"  -- 공백문자도 특수문자 
-FROM employees;
+SELECT
+    employee_id empno, -- 컬럼명 뒤에 별칭
+    first_name  AS "f-name", -- as 별칭, 특수문자 포함되면 ""로 묶음
+    salary      "급 여"  -- 공백문자도 특수문자 
+FROM
+    employees;
 
 
 -- 직원 이름(first_name last_name 합쳐서) name
 -- 급여(커미션이 포함된 급여), 급여 * 12 연봉 별칭으로 표기
-SELECT first_name || ' ' || last_name "Full Name" 
-, salary + salary * nvl(commission_pct, 0) "급여(커미션포함)" 
-, salary * 12 연봉
-FROM employees;
+SELECT
+    first_name
+    || ' '
+    || last_name "Full Name",
+    salary + salary * nvl(
+        commission_pct, 0
+    )            "급여(커미션포함)",
+    salary * 12  연봉
+FROM
+    employees;
 
 -- 연습 Alias 붙이기 
-SELECT first_name || ' ' || last_name 이름, 
-hire_date 입사일, 
-phone_number 전화번호, 
-salary 급여, 
-salary * 12 연봉
-FROM employees;
+SELECT
+    first_name
+    || ' '
+    || last_name 이름,
+    hire_date    입사일,
+    phone_number 전화번호,
+    salary       급여,
+    salary * 12  연봉
+FROM
+    employees;
 
 
 --------
@@ -97,53 +160,93 @@ FROM employees;
 -- 비교연산 : =, <>, >, >=, <, <=
 
 -- 사원들 중, 급여가 15000 이상인 직원의 이름과 급여 
-SELECT  first_name, salary
-FROM employees
-WHERE salary > 15000;
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary > 15000;
     
 -- 입사일이 07/01/01 이후인 직원들의 이름과 입사일 
-SELECT first_name, hire_date
-FROM employees
-WHERE hire_date >= '15/01/01';
+SELECT
+    first_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date >= '15/01/01';
     
 -- 급여가 14000 이하이거나, 17000 이상인 사원의 이름과 급여
-SELECT first_name, salary
-FROM employees
-WHERE salary <= 4000 OR salary >= 17000;
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary <= 4000
+    OR salary >= 17000;
     
 -- 급여가 14000 이상이고, 17000 이하인 사원의 이름과 급여
-SELECT first_name, salary
-FROM employees
-WHERE salary <= 17000 AND salary >= 14000;
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary <= 17000
+    AND salary >= 14000;
     
 -- BETWEEN : 범위 비교
-SELECT first_name, salary
-FROM employees
-WHERE salary BETWEEN 14000 and 17000;
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary BETWEEN 14000 AND 17000;
 
 -- NULL 체크 =, <> 사용하면 안됨
 -- IS NULL, IS NOT NULL
 
 -- commission을 받지 않는 사람들 (-> commission_pct 가 null인 레코드)
-SELECT first_name, commission_pct
-FROM employees
-WHERE commission_pct IS NULL;   -- NULL 체크
+SELECT
+    first_name,
+    commission_pct
+FROM
+    employees
+WHERE
+    commission_pct IS NULL;   -- NULL 체크
 
 -- commission을 받는 사람들 (-> commission_pct가 널이 아닌 레코드) IS NOT NULL
-SELECT first_name, commission_pct
-FROM employees
-WHERE commission_pct IS NOT NULL;   -- NULL 체크
+SELECT
+    first_name,
+    commission_pct
+FROM
+    employees
+WHERE
+    commission_pct IS NOT NULL;   -- NULL 체크
 
 -- IN 연산자 : 특정 집합의 요소와 비교 
 -- 사원들 중 10, 20, 40번 부서에서 근무하는 직원들의 이름과 부서 아이디
-SELECT first_name, department_id 
-FROM employees
-WHERE department_id = 10 OR department_id = 20 OR department_id = 40;
+SELECT
+    first_name,
+    department_id
+FROM
+    employees
+WHERE
+    department_id = 10
+    OR department_id = 20
+    OR department_id = 40;
 
 -- IN 연산자 : 특정 집합의 요소와 비교
-SELECT first_name, department_id 
-FROM employees
-WHERE department_id IN (10, 20, 40);
+SELECT
+    first_name,
+    department_id
+FROM
+    employees
+WHERE
+    department_id IN ( 10, 20, 40 );
 
 -- LIKE 연산자 
 -- ename LIKE 'KOR%' : KOR로 시작하는 모든 문자열 (KOR 포함)
@@ -158,21 +261,127 @@ WHERE department_id IN (10, 20, 40);
 -- _ : 1개의 정해지지 않은 문자
 
 -- 이름에 am을 포함하고 있는 사원의 이름과 급여 
-SELECT first_name, salary
-FROM employees
-WHERE LOWER(first_name) LIKE '%am%';
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    lower(first_name) LIKE '%am%';
 
 -- 이름의 두번째 글자가 a인 사원의 이름과 급여
-SELECT first_name, salary
-FROM employees
-WHERE LOWER(first_name) LIKE '_a%';
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    lower(first_name) LIKE '_a%';
 
 -- 이름의 네번째 글자가 a인 사원의 이름과 급여
-SELECT first_name, salary
-FROM employees
-WHERE LOWER(first_name) LIKE '___a%';
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    lower(first_name) LIKE '___a%';
 
 -- 이름이 네글자인 사원들 중에서 두번째 글자가 a인 사원의 이름과 급여
-SELECT first_name, salary
-FROM employees
-WHERE LOWER(first_name) LIKE '_a__';
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    lower(first_name) LIKE '_a__';
+
+
+-- 쿼리 연습
+-- 부서 ID가 90인 사원 중, 급여가 20000 이상인 사원 (이름, 급여)
+SELECT
+    first_name,
+    salary,
+    department_id
+FROM
+    employees
+WHERE
+    department_id IN 90
+    AND salary >= 20000;
+
+-- 입사일이 11/01/01 ~ 17/12/31 구간에 있는 사원의 목록 (이름, 입사일)
+SELECT
+    first_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date >= '11/01/01'
+    AND hire_date <= '17/12/31';
+
+SELECT
+    first_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date BETWEEN '11/01/01' AND '17/12/31';
+
+-- manager_id가 100, 120, 147인 사원의 목록 (이름, 매니저 아이디)
+SELECT
+    first_name,
+    manager_id
+FROM
+    employees
+WHERE
+    manager_id IN ( 100, 120, 147 );
+
+SELECT
+    first_name,
+    manager_id
+FROM
+    employees
+WHERE
+    manager_id = 100
+    OR manager_id = 120
+    OR manager_id = 147;
+--  두 쿼리를 비교
+
+----------------
+-- ORDER BY ----
+----------------
+
+-- 특정 컬럼명, 연산식, 별칭, 컬럼 순서를 기준으로 레코드 정렬
+-- ASC(오름차순: default) , DESC (내림차순)
+-- 여러개의 컬럼에 적용할 수 있고 ','로 구분
+
+-- 부서 번호 오름차순으로 정렬하고, 부서 번호와 급여, 이름 출력
+SELECT
+    department_id,
+    first_name
+FROM
+    employees
+ORDER BY
+    department_id ASC;
+
+-- 급여가 10000 이상인 직원 대상, 급여의 내림차순으로 출력, 이름, 급여
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary >= 10000
+ORDER BY
+    salary DESC;
+
+-- 부서 번호, 급여, 이름순으로 출력, 정렬 기준 부서번호 오름차순, 급여 내림차순 
+SELECT
+    department_id,
+    salary,
+    first_name
+FROM
+    employees
+ORDER BY
+    department_id ASC,
+    salary DESC;
